b2110525a57085845c33156c2e6c9c2ce7534dd1
VRIEN-VEI <sylvia.olafsen@outlook.com>
2023-01-17 13:41:06 +0100

    Homepage

---
+++ script/navbar.js
@@ -0,0 +1,47 @@
+
+/* Source: Ladd, A. [alstudiowebdev] (2022, June 14). 
+Responsive and Accessible Navigation (HTML, CSS, Vanilla JS) [Video]. 
+Youtube. https://youtu.be/IdZ-ZHLyEHs */
+
+const mobileMenuTrigger = document.getElementById('mobileMenuTrigger');
+const mobileMenu = document.querySelector('.nav-links-container');
+const navLinks = document.querySelectorAll('.nav-links li a');
+
+mobileMenuTrigger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+    mobileMenuTrigger.classList.toggle('active');
+
+    // Toggle Aria-Expanded Attribute True / False
+    if (mobileMenu.classList.contains('active')) {
+        mobileMenuTrigger.setAttribute('aria-expanded', 'true');
+    } else {
+        mobileMenuTrigger.setAttribute('aria-expanded', 'false');
+    }
+});
+
+// Provide Styling for Current Page Anchor Tag
+
+navLinks.forEach((link) => {
+    if (link.href === window.location.href) {
+        link.setAttribute('aria-current', 'page');
+    }
+});
+
+// Make index.html page active on page load
+
+const baseURI = document.baseURI;
+const homePageAnchor = document.getElementById('home');
+
+window.addEventListener('load', () => {
+    if (baseURI) {
+        homePageAnchor.setAttribute('aria-current', 'page');
+    }
+});
+
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        mobileMenu.classList.remove('active');
+        mobileMenuTrigger.classList.remove('active');
+    }
+});
