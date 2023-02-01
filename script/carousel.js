5dd699c28012c7257e947c52260167bf598f3ebe
VRIEN-VEI <sylvia.olafsen@outlook.com>
2023-02-01 16:20:29 +0100

    ...

--- script/carousel.js
+++ script/carousel.js
@@ -13,15 +13,15 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+    let line = document.getElementsByClassName("line");
     if (n > slides.length) {slideIndex = 1}    
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
     }
-    for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < line.length; i++) {
+    line[i].className = line[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    line[slideIndex-1].className += " active";
 }
